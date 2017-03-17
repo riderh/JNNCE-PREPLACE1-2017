5a9eb5f4555abdd1933d9fbcd16c7beb4c478c4b
madhushirali <mrshirali1296@gmail.com>
2017-03-18 01:37:19 +0530

    for swap

--- cprograms/swap/main.c
+++ cprograms/swap/main.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
-void main()
+int main()
 {
-    int a,b;
-    printf("enter the two numbers\n");
-
+int a=0,b=0;
+printf("ENTER VALUES FOR a AND b\n");
+scanf("%d %d",&a,&b);
+printf("BEFORE SWAPING a=%d and b=%d\n",a,b);
+a=a+b;
+b=a-b;
+a=a-b;
+printf("AFTER SWAPING a=%d and b=%d\n",a,b);
+return 0;
 }
