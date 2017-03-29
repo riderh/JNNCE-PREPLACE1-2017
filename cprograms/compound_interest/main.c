2faf3859259f31d3b318f28755ee462c71a63bf0
madhushirali <mrshirali1296@gmail.com>
2017-03-29 14:46:17 +0530

    FOR COMPOUND INTEREST

---
+++ cprograms/compound_interest/main.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float P,CI,r,t,k;
+    int n;
+    printf("ENTER THE PRINCIPLE AMOUNT\n");
+    scanf("%f",&P);
+    printf("ENTER THE ANNUAL RATE OF INTEREST IN TERMS OF PERENTAGE\n");
+    scanf("%f",&r);
+    printf("ENTER THE TIME SPAN IN TERMS OF YEARS\n");
+    scanf("%f",&t);
+    printf("ENTER THE NUMBER OF YEARSTHE INTEREST IS COMPOUNDED IN ONE YEAR\n");
+    scanf("%d",&n);
+    k=r/100;
+    CI=P*pow((1+(k/n)),(n*t))-P;
+    printf("THE COMPOUND INTEREST IS %f\n",CI);
+    return 0;
+}
