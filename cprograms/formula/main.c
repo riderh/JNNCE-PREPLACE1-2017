1c9ec5c1f59a8cea6c550c84efd76f65f2764d0e
madhushirali <mrshirali1296@gmail.com>
2017-04-01 15:58:30 +0530

    FOR FORMULA PROOF

--- cprograms/formula/main.c
+++ cprograms/formula/main.c
@@ -7,15 +7,17 @@ int main()
     scanf("%f%f",&a,&b);
     p=(a+b)*(a+b);
     q=pow(a,2)+(2*a*b)+pow(b,2);
-    if(p=q)
+    if(p==q)
         printf("(a+b)^2=a^2+2ab+b^2 is proved\n");
     else
         printf("(a+b)^2=a^2+2ab+b^2 is not proved\n");
     r=(a-b)*(a-b);
     s=pow(a,2)-(2*a*b)+pow(b,2);
-    if(r=s)
+    if(r==s)
         printf("(a+b)^2=a^2-2ab+b^2 is proved\n");
     else
         printf("(a+b)^2=a^2-2ab+b^2 is not proved\n");
     return 0;
 }
+
+
