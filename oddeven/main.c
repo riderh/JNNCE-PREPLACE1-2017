12d090a58179c45102f6b22b20c18374e112ce8c
JeevithaHM <jeevithamanjunath9@gmail.com>
2017-04-05 16:28:01 +0530

    odd

---
+++ oddeven/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,x;
+    printf("enter the odd \n");
+    scanf("%d",&x);
+    printf("the odd no in the range of\n");
+  for( i=1;i<=x;i+=2)
+        printf("%d\n",i);
+    return 0;
+}
