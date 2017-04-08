80abb4042d97cb62f78187480e7203cf272e64ac
JeevithaHM <jeevithamanjunath9@gmail.com>
2017-04-08 19:55:46 +0530

    compare of two numbers

---
+++ compare2num/main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    float x,y;
+    printf("enter the two numbers\n");
+    scanf("%f%f",&x,&y);
+    if(x>y)
+        printf("x is greater than b\n");
+    else if(x==y)
+
+         printf("x is equal to b\n");
+    else
+        printf("x is less than y\n");
+}
