7d68bc7a9f403af0ce1e3a570cbd85cfcba5528c
AnushaMR <anushamr1996@gmail.com>
2017-04-12 09:35:31 -0700

    for exampersentage

---
+++ CPGMS/exampersentage/main.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+float p;
+printf("ENTER YOUR PERCENTAGE \n");
+scanf("%f",&p);
+if(p>=70.0)
+printf("DISTINCTION\n");
+else if(p>=60.0)
+printf("FIRST CLASS\n");
+else if(p>=50.0)
+printf("SECOND CLASS\n");
+else if(p>=35.0)
+printf("PASS CLASS\n");
+else
+printf("FAIL\n");
+}
+
+
